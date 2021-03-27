de4426b8304b5ae339980b97635f8b0ccc1b3798
Tom Sitter <thomas.sitter@gmail.com>
2021-03-27 13:37:12 -0700

    Create flask app

--- app.py
+++ app.py
@@ -1,44 +1,26 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pydeck as pdk
-from pydeck.types import String
-import math
-from ast import literal_eval
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-@st.cache
-def load_data():
-    #dtype = {"diff_" + str(year): np.int64 for year in range(2001,2020)}
+@app.route('/name/', methods=['GET'])
+def respond():
+    name = request.args.get("name", None)
+    print(f"got name {name}")
+
+    response = {}
     
-    dtype = {'lat': np.float64, 'lon': np.float64}
-    data =  pd.read_csv('data/cities.csv', dtype=dtype, converters={f'color_spec_{year}': literal_eval for year in range(2002,2020)})
-    for year in range(2002,2020):
-        data['radius_'+str(year)] = data['scaled_diff_'+str(year)].abs()*250_000
-    return data
+    if not name:
+        response["ERROR"] = "no name found, please send a name"
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric"
+    else:
+        response["MESSAGE"] = f"Welcome {name}"
 
-data = load_data()
+    return jsonify(response)
 
-st.title("Map of cities of Canada")
-st.write(data)
-year = st.slider("Year", min_value=2002, max_value=2019, value=2002, step=1)
-r = pdk.Deck(
-    map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state = pdk.ViewState(
-        latitude = 49.89,
-        longitude = -97.13,
-        zoom = 3,
-        pitch =  0,
-    ),
-    layers=[pdk.Layer(
-        "ScatterplotLayer",
-        data,
-        opacity=0.5,
-        get_position=["lon", "lat"],
-        get_radius="radius_" + str(year),
-        get_fill_color="color_spec_" + str(year),
-        pickable=True,
-    )],
-    tooltip={"text": "{city_name}"},
-)
-st.pydeck_chart(r)
+@app.route('/')
+def index():
+    return "<h1>Welcome to use server</h1>"
 
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
